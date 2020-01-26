74132ce3cdc2308196bf9aed821c0258e46b7d24
Kenneth <kennethgarces@hotmail.com>
2020-01-25 18:54:30 -0800

    Remove output file

--- run.sh
+++ run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-echo "Enter step one"
-read args1
-echo "Enter step two"
-read args2
-echo "Enter step three"
-read args3
-echo "Enter step three"
-read args4
 
 cd /Users/kgarces/Projects/HelloWorldDotNet/DotNetHelloWorld/DotNetHelloWorld
 echo "Process .NET pipeline has started!!!."
-dotnet run $args1 $args2 $args3 $args4 >> SampleOut-$(date +"%Y_%m_%d_%I_%M_%p").log
+dotnet run build unit test deploy
+#dotnet run $args1 $args2 $args3 $args4 >> SampleOut-$(date +"%Y_%m_%d_%I_%M_%p").log

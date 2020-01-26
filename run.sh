e74d05a43ce4efc1293d200b5e1eeb85d413a71a
Kenneth <kennethgarces@hotmail.com>
2020-01-25 19:23:04 -0800

    Troubleshooting dotnet

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@
 
 cd /Users/kgarces/Projects/HelloWorldDotNet/DotNetHelloWorld/DotNetHelloWorld
 echo "Process .NET pipeline has started!!!."
-dotnet restore
-dotnet build
 dotnet run build unit test deploy
 

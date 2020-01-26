e5d66d607c4b8e4d2d2069e47efd8f0d933eeffb
Kenneth <kennethgarces@hotmail.com>
2020-01-25 19:10:26 -0800

    Update dotnet restore

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 cd /Users/kgarces/Projects/HelloWorldDotNet/DotNetHelloWorld/DotNetHelloWorld
 echo "Process .NET pipeline has started!!!."
+dotnet restore
 dotnet build
 dotnet run build unit test deploy
 

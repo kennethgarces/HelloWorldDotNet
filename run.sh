c62debcc3424c4f386afd605b3b151fc936851c9
Kenneth <kennethgarces@hotmail.com>
2020-01-25 18:49:26 -0800

    Fix Sample output

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ read args4
 
 cd /Users/kgarces/Projects/HelloWorldDotNet/DotNetHelloWorld/DotNetHelloWorld
 echo "Process .NET pipeline has started!!!."
-dotnet run $args1 $args2 $args3 $args4 >> ../../SampleOut-$(date +"%Y_%m_%d_%I_%M_%p").log
+dotnet run $args1 $args2 $args3 $args4 >> SampleOut-$(date +"%Y_%m_%d_%I_%M_%p").log

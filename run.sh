a5c021b6568a6b80584b98d1b09ebd599b4967d7
Kenneth <kennethgarces@hotmail.com>
2020-01-25 19:03:59 -0800

    fix run

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+$args1=('build')
+$args2=('unit')
+$args3=('test')
+$args4=('deploy')
 
 cd /Users/kgarces/Projects/HelloWorldDotNet/DotNetHelloWorld/DotNetHelloWorld
 echo "Process .NET pipeline has started!!!."
-dotnet run build unit test deploy
-#dotnet run $args1 $args2 $args3 $args4 >> SampleOut-$(date +"%Y_%m_%d_%I_%M_%p").log
+dotnet build
+dotnet run $args1 $args2 $args3 $args4
+
